7b1e9256a4659a220f26ea73a04324864224085a
Fernando Costa Bertoldi <fcbertoldi@gmail.com>
2024-04-24 16:20:21 -0300

    Add model Contact

--- core/models.py
+++ core/models.py
@@ -1,5 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
 class Contact(models.Model):
-    uuid = models.UUIDField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first = models.CharField(max_length=100)
+    last = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=30, null=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["id"]),
+        ]
