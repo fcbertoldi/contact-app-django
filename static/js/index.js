aa4ef94dd32eff72c29ad5ea9e74bc863fafd56e
Fernando Costa Bertoldi <fcbertoldi@gmail.com>
2024-05-17 09:36:00 -0300

    Fix auto-download function in index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,12 @@
-function autoDownloadLink(elt) {
-    elt.querySelectorAll("[data-auto-download]").forEach(element => {
-        element.click();
-    });
+function downloadFiles(loadTarget) {
+    if (loadTarget.hasAttribute('data-download-load-target')) {
+        loadTarget.querySelectorAll("[data-download-file]").forEach(element => {
+            element.click();
+        });
+    }
 }
 
+
 document.addEventListener("DOMContentLoaded", (event) => {
-    document.addEventListener("htmx:load", e => autoDownloadLink(e.target));
+    document.addEventListener("htmx:load", e => downloadFiles(e.target));
 });
