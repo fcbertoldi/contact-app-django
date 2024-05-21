c5aae35609cb6ff085f2076bc3a922ec27ee16c5
Fernando Costa Bertoldi <fcbertoldi@gmail.com>
2024-05-21 13:56:56 -0300

    Integrate SweetAlert2 library

--- static/js/index.js
+++ static/js/index.js
@@ -1,79 +1,89 @@
+function sweetConfirm(elt, config) {
+  Swal.fire(config)
+    .then((result) => {
+      if (result.isConfirmed) {
+        console.log('FOOOO');
+        elt.dispatchEvent(new Event('confirmed', { bubbles: true }));
+      }
+    });
+}
+
 function downloadFiles(loadTarget) {
-    if (loadTarget.hasAttribute('data-download-load-target')) {
-        loadTarget.querySelectorAll("[data-download-file]").forEach(element => {
-            element.click();
-        });
-    }
+  if (loadTarget.hasAttribute('data-download-load-target')) {
+    loadTarget.querySelectorAll("[data-download-file]").forEach(element => {
+      element.click();
+    });
+  }
 }
 
 function overflowMenu(subtree = document) {
-    subtree.querySelectorAll("[data-overflow-menu]").forEach(menuRoot => {
-      const
+  subtree.querySelectorAll("[data-overflow-menu]").forEach(menuRoot => {
+    const
       button = menuRoot.querySelector("[aria-haspopup]"),
       menu = menuRoot.querySelector("[role=menu]"),
       items = [...menu.querySelectorAll("[role=menuitem]")];
 
-      const isOpen = () => !menu.hidden;
+    const isOpen = () => !menu.hidden;
 
-      items.forEach(item => item.setAttribute("tabindex", "-1"));
+    items.forEach(item => item.setAttribute("tabindex", "-1"));
 
-      function toggleMenu(open = !isOpen()) {
-        if (open) {
-          menu.hidden = false;
-          button.setAttribute("aria-expanded", "true");
-          items[0].focus();
-        } else {
-          menu.hidden = true;
-          button.setAttribute("aria-expanded", "false");
-        }
+    function toggleMenu(open = !isOpen()) {
+      if (open) {
+        menu.hidden = false;
+        button.setAttribute("aria-expanded", "true");
+        items[0].focus();
+      } else {
+        menu.hidden = true;
+        button.setAttribute("aria-expanded", "false");
       }
+    }
 
-      toggleMenu(isOpen());
-      button.addEventListener("click", () => toggleMenu());
-      menuRoot.addEventListener("blur", e => console.log(e) || toggleMenu(false));
+    toggleMenu(isOpen());
+    button.addEventListener("click", () => toggleMenu());
+    menuRoot.addEventListener("blur", e => console.log(e) || toggleMenu(false));
 
-      window.addEventListener("click", function clickAway(event) {
-        if (!menuRoot.isConnected) window.removeEventListener("click", clickAway);
-        if (!menuRoot.contains(event.target)) toggleMenu(false);
-      })
-
-      const currentIndex = () => {
-        const idx = items.indexOf(document.activeElement);
-        if (idx === -1) return 0;
-        return idx;
-      }
-
-      menuRoot.addEventListener("keydown", e => {
-        if (e.key === "ArrowUp") {
-          items[currentIndex() - 1]?.focus();
-
-        } else if (e.key === "ArrowDown") {
-          items[currentIndex() + 1]?.focus();
-
-        } else if (e.key === "Space") {
-          items[currentIndex()].click();
-
-        } else if (e.key === "Home") {
-          items[0].focus();
-
-        } else if (e.key === "End") {
-          items[items.length - 1].focus();
-
-        } else if (e.key === "Escape") {
-          toggleMenu(false);
-          button.focus();
-
-        } else if (e.key === "Tab") {
-          toggleMenu(false);
-        }
-      })
+    window.addEventListener("click", function clickAway(event) {
+      if (!menuRoot.isConnected) window.removeEventListener("click", clickAway);
+      if (!menuRoot.contains(event.target)) toggleMenu(false);
     })
-  }
+
+    const currentIndex = () => {
+      const idx = items.indexOf(document.activeElement);
+      if (idx === -1) return 0;
+      return idx;
+    }
+
+    menuRoot.addEventListener("keydown", e => {
+      if (e.key === "ArrowUp") {
+        items[currentIndex() - 1]?.focus();
+
+      } else if (e.key === "ArrowDown") {
+        items[currentIndex() + 1]?.focus();
+
+      } else if (e.key === "Space") {
+        items[currentIndex()].click();
+
+      } else if (e.key === "Home") {
+        items[0].focus();
+
+      } else if (e.key === "End") {
+        items[items.length - 1].focus();
+
+      } else if (e.key === "Escape") {
+        toggleMenu(false);
+        button.focus();
+
+      } else if (e.key === "Tab") {
+        toggleMenu(false);
+      }
+    })
+  })
+}
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    document.addEventListener("htmx:load", e => {
-        downloadFiles(e.target);
-        overflowMenu(e.target);
-    });
+  document.addEventListener("htmx:load", e => {
+    downloadFiles(e.target);
+    overflowMenu(e.target);
+  });
 });
